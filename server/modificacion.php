6d6f05a4d6afe72bfe93ca62a00df6bdbc89bfe7
luciogayoso <luciogayoso@gmail.com>
2023-10-14 23:25:44 -0300

    Add files via upload

--- server/modificacion.php
+++ server/modificacion.php
@@ -1,49 +1,54 @@
-
+<!DOCTYPE html>
 <html lang="es">
-</html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Server altas</title>
+	<link rel="stylesheet" href="../css/respuesta-alta.css">
+</head>
+
+<?php
 
 
-<?php 
+include("../modelo/usuario.php");
 
-include 'menu.php';
-
-
+$use = $_POST['usuario'];
+$cla = $_POST['clave'];
 $ape = $_POST['apellido'];
 $nom = $_POST['nombre'];
-$ed = $_POST['edad'];
+$fe = $_POST['fecha'];
+$foto = $_FILES["foto"]["tmp_name"];
+$fotoTamanio = $_FILES["foto"]["size"];
 
-$base = "gestionsubir";
-$Conexion =  mysqli_connect("localhost","root","",$base);
-if($Conexion){
-	echo "la conexion fue exitosa "."<br>";
+$result = modificar($use, $cla, $ape, $nom, $fe, $foto, $fotoTamanio);
 
-}else{
-	echo "la conexion ha fallado "."<br>";
+if (!$result) {
+	echo '<div class="Rcontainer">
+    <div class="Rbox">
+        <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
+        <h3 class="Rcuerpo">' . $result . '</h3>
+        <a href="../from/menu.php" class="cerrar">Cerrar</a>
+    </div>
+</div>';
+} else {
+	return include("../from/respuestas-server/respuesta-modificar.php");
 }
-
-$cadena = "UPDATE  persona SET nombre = '$nom', edad = '$ed' WHERE apellido = '$ape'";
-
-$resultado = mysqli_query($Conexion,$cadena);
-
-if($resultado){
-	echo "se ha modificado un registro"."<br>";
-
-}else{
-	echo "NO se ha modificado un registro"."<br>";
-	echo mysqli_error($Conexion);
-}
- ?>
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
+
 <body>
 	<a href="form-modificacion.php">Volver</a>
-	
+
 </body>
+
 </html>
