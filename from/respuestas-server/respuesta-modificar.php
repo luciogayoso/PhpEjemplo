55509aa41bccbfb7dcce0b71d48f4b90911028fe
luciogayoso <luciogayoso@gmail.com>
2023-10-14 23:18:43 -0300

    Add files via upload

--- from/respuestas-server/respuesta-modificar.php
+++ from/respuestas-server/respuesta-modificar.php
@@ -1,7 +1,7 @@
 <div class="Rcontainer">
     <div class="Rbox">
         <h2 class="Rtitulo">Esta es una respuesta del servidor</h2>
-        <h3 class="Rcuerpo">El registros se elimino correctamente</h3>
+        <h3 class="Rcuerpo">El registros se modifico correctamente</h3>
         <a href="../from/menu.php" class="cerrar">Cerrar</a>
     </div>
 </div>
